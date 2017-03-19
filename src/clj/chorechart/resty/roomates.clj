cb5cc9d07f3cfd129ba39a30572349f677b649c5
jgoodhcg <jgoodhcg@gmail.com>
2017-03-19 16:37:43 -0400

    case insensitive email

--- src/clj/chorechart/resty/roomates.clj
+++ src/clj/chorechart/resty/roomates.clj
@@ -1,16 +1,19 @@
 (ns chorechart.resty.roomates
   (:require [chorechart.db.core :as db]
             [ring.util.http-response :as response]
+            [clojure.string :as str]
             ))
 
 (defn view [params]
   (db/list-roomates {:living_situation_id (:living_situation_id params)}))
 
 (defn add [params]
-  (let [{:keys [roomate_email living_situation_id]} params]
-    (if-let [person (db/find-person {:email roomate_email})]
+  (let [{:keys [roomate_email living_situation_id]} params
+        roomate_email_no_case (str/lower-case roomate_email)]
+
+    (if-let [person (db/find-person {:email roomate_email_no_case})]
       (let [new_living_situation_id (db/add-roomate!
-                                     {:roomate_email roomate_email
+                                     {:roomate_email roomate_email_no_case
                                       :living_situation_id living_situation_id})]
         (list (assoc (select-keys person [:user_name])
                      :living_situation new_living_situation_id)))
